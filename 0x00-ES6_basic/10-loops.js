e75d13ef2b73ed689b18d64733f653d015585258
Sufi-to <3thomas225@gmail.com>
2024-07-23 03:28:22 +0400

    map the appended string

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    const index = array.indexOf(idx);
-    array[index] = appendString + idx;
-  }
-  return array;
+  return array.map((value) => appendString + value);
 }
