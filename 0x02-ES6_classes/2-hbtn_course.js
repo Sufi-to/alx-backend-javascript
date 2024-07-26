85b6f2f3f46a9b0f4b7fb7c7ec939b6592be3ef9
Sufi-to <3thomas225@gmail.com>
2024-07-26 04:42:09 +0400

    fixed the constructor

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,8 @@
 export default class HolbertonCourse {
-  constructor(name, length, students) {
+  constructor(name, length = 0, students) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
