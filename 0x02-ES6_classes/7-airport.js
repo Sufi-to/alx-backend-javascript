2c13d5a88d1eac6dff5eb40b8f09844cd0ace795
Sufi-to <3thomas225@gmail.com>
2024-07-26 04:03:52 +0400

    all classes

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(setName) {
+    if (typeof setName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = setName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(setCode) {
+    if (typeof setCode !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = setCode;
+  }
+
+  toString() {
+    return `Airport [${this._code}] ${this._name}`;
+  }
+}
