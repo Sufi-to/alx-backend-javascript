c0a079e949894eba6546bbf4e59f0bc384f973c9
Sufi-to <3thomas225@gmail.com>
2024-07-30 23:26:54 +0400

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,8 +2,8 @@ export default function cleanSet(set, startString) {
   if (startString.length === 0 || typeof set !== 'object' || typeof startstring !== 'string') {
     return '';
   }
-  return [...set]
-    .filter((word) => (word !== 'undefined' ? word.startsWith(startString) : ''))
-    .map((word) => (word !== 'undefined' ? word.slice(startString.length) : ''))
-    .join('-');
+  const wordStartWith = [...set]
+    .filter((word) => word.startsWith(startString))
+    .map((word) => word.slice(startString.length));
+  return wordStartWith.join('-');
 }
