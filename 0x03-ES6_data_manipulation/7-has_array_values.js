dad22eee0c87c84730325b05ad2570f146e5189b
Sufi-to <3thomas225@gmail.com>
2024-08-02 16:58:31 +0400

    are all included

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,10 @@
 export default function hasValuesFromArray(set, array) {
-  return set.isSupersetOf(new Set(array));
+  let res = true;
+  array.map((element) => {
+    if (!set.has(element)) {
+      res = false;
+    }
+    return '';
+  });
+  return res;
 }
