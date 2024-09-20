890f1c8800004f04be5c5c7b037986f3ec350eae
Sufi-to <3thomas225@gmail.com>
2024-09-21 03:33:17 +0400

    argument change

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,7 +1,7 @@
 const Utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const result = Utils.calculateNumber('SUM', totalShipping, totalAmount);
+  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${result}`);
 }
 
