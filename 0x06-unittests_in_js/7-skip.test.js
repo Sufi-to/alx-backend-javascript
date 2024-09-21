fee1532d1086a5cd192c280c7877ca3de4a9cb85
Sufi-to <3thomas225@gmail.com>
2024-09-21 04:50:02 +0400

    skip tests

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 
 describe('Testing numbers', () => {
-  it.skip('1 is equal to 1', () => {
+  it('1 is equal to 1', () => {
     expect(1 === 1).to.be.true;
   });
 
@@ -9,7 +9,7 @@ describe('Testing numbers', () => {
     expect(2 === 2).to.be.true;
   });
 
-  it('1 is equal to 3', () => {
+  it.skip('1 is equal to 3', () => {
     expect(1 === 3).to.be.true;
   });
 
@@ -21,7 +21,7 @@ describe('Testing numbers', () => {
     expect(4 === 4).to.be.true;
   });
 
-  it.skip('5 is equal to 5', () => {
+  it('5 is equal to 5', () => {
     expect(5 === 5).to.be.true;
   });
 
